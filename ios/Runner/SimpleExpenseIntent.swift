0a11c4db4771098a7ad03e811cd2e7052d6806e8
Fareez <fareez.jasmi@myeg.com.my>
2025-09-19 17:21:56 +0800

    Update

--- ios/Runner/SimpleExpenseIntent.swift
+++ ios/Runner/SimpleExpenseIntent.swift
@@ -3,7 +3,7 @@ import Foundation
 
 // Simple test intent with no parameters for easier Siri discovery
 @available(iOS 16.0, *)
-struct SimpleExpenseIntent: AppIntent, ProvidesDialog {
+struct SimpleExpenseIntent: AppIntent {
   static var title: LocalizedStringResource = "Add Expense"
   static var description: LocalizedStringResource = "Add a $5 expense to your ledger"
   static var openAppWhenRun: Bool = false // Don't open app, just run in background
@@ -21,8 +21,7 @@ struct SimpleExpenseIntent: AppIntent, ProvidesDialog {
         description: "Added via Siri"
     )
 
-    return .result(
-      dialog: IntentDialog("Added $5.00 expense to your ledger")
-    )
+    // Return success without value
+    return .result()
   }
 }
